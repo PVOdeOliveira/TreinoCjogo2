39c2863da24ad0aa252e9e7a9e4163de4cc09240
PVOdeOliveira <p.vitor.oliveira.pvo@gmail.com>
2022-03-02 00:58:21 -0300

    adicionando a funcao que imprime na tela a palavra revelada

--- Forca.c
+++ Forca.c
@@ -4,13 +4,13 @@
 char palavra[20], letrasTentadas[26];
 int tentativa=0;
 
-void titulo(){
+void titulo(){ //mostra um titulo
     printf("|*****************|\n");
     printf("|* Jogo da Forca *|\n");
     printf("|*****************|\n\n");
 }
 
-void jogada(){
+void jogada(){ //recebe a letra jogada em cada rodada e armazena em um array com todas as tentativas
     char letra;
 
     printf("Digite uma letra: ");
@@ -21,6 +21,8 @@ void jogada(){
 }
 
 int testaCerto(int tentativa, char letrasTentadas[26], char letraCerta){
+    //compara a letra da jogada atual, ja armazenada no array de tentativas, com a letra certa da posicao testada
+
     int acertou=0, j;
 
     for(j=0;j<tentativa;j++){
@@ -31,28 +33,34 @@ int testaCerto(int tentativa, char letrasTentadas[26], char letraCerta){
     return acertou;
 }
 
+void imprimePalavra(){//imprime todas as letras descobertas até o momento
+    int i, acertou;
+    for(i=0;i<strlen(palavra);i++){
+
+        acertou = testaCerto(tentativa, letrasTentadas, palavra[i]);
+
+        if(acertou==1){
+            printf(" %c",palavra[i]);
+        }
+        else{
+            printf(" _");
+        }
+    }
+    printf("\n\n");
+}
+
+
 int main() {
 
-    int i, acertou, venceu=0, enforcou=0;
+    int venceu=0, enforcou=0;
 
     sprintf(palavra,"MELANCIA"); //Palavra fixa para testes
 
     titulo();
 
     while(venceu==0 && enforcou==0){
-        for(i=0;i<strlen(palavra);i++){
 
-
-            acertou = testaCerto(tentativa, letrasTentadas, palavra[i]);
-
-            if(acertou==1){
-                printf(" %c",palavra[i]);
-            }
-            else{
-                printf(" _");
-            }
-        }
-        printf("\n\n");
+        imprimePalavra();
 
         jogada(letrasTentadas,tentativa);
 
